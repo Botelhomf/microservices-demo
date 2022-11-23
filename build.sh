67723ae092744ce34b883d52c3e0325394f7af1e
Botelhomf <danibmf@gmail.com>
2022-11-23 01:49:58 +0000

    coisas de hoje 2211

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker build src/emailservice -t emailservice
-docker build src/productcatalogservice -t productcatalogservice
-docker build src/recommendationservice -t recommendationservice
-docker build src/shippingservice -t shippingservice
-docker build src/checkoutservice -t checkoutservice
-docker build src/paymentservice -t paymentservice
-docker build src/currencyservice -t currencyservice
-docker build src/cartservice/src -t cartservice
-docker build src/frontend -t frontend
-docker build src/adservice -t adservice
-docker build src/loadgenerator -t loadgenerator
+docker build src/emailservice -t gcr.io/$PROJECT_ID/emailservice
+docker build src/productcatalogservice -t gcr.io/$PROJECT_ID/productcatalogservice
+docker build src/recommendationservice -t gcr.io/$PROJECT_ID/recommendationservice
+docker build src/shippingservice -t gcr.io/$PROJECT_ID/shippingservice
+docker build src/checkoutservice -t gcr.io/$PROJECT_ID/checkoutservice
+docker build src/paymentservice -t gcr.io/$PROJECT_ID/paymentservice
+docker build src/currencyservice -t gcr.io/$PROJECT_ID/currencyservice
+docker build src/cartservice/src -t gcr.io/$PROJECT_ID/cartservice
+docker build src/frontend -t gcr.io/$PROJECT_ID/frontend
+docker build src/adservice -t gcr.io/$PROJECT_ID/adservice
+docker build src/loadgenerator -t gcr.io/$PROJECT_ID/loadgenerator
---
+++ push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+docker push gcr.io/my-project-2211-369500/emailservice
+docker push gcr.io/my-project-2211-369500/productcatalogservice
+docker push gcr.io/my-project-2211-369500/recommendationservice
+docker push gcr.io/my-project-2211-369500/shippingservice
+docker push gcr.io/my-project-2211-369500/checkoutservice
+docker push gcr.io/my-project-2211-369500/paymentservice
+docker push gcr.io/my-project-2211-369500/currencyservice
+docker push gcr.io/my-project-2211-369500/cartservice
+docker push gcr.io/my-project-2211-369500/frontend
+docker push gcr.io/my-project-2211-369500/adservice
+docker push gcr.io/my-project-2211-369500/loadgenerator
+
+
+
+
+
+
+
+
+
+
